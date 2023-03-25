631ff7672a16348245341aae6d6572bb8f78c19a
kirasir1 <zebra46@gmail.com>
2023-03-25 12:44:11 +0300

    Продолжил писать

--- main.py
+++ main.py
@@ -13,15 +13,42 @@ async def handler(event):
 
 @bot.on(events.CallbackQuery(data=b'1'))
 async def send_command_handler(send_event):
-    payload = {}
+    workday = {}
     await send_event.answer()
     day = getweek()
     kbdays = [[Button.inline(day[0], b"d1")],[Button.inline(day[1], b"d2")],[Button.inline(day[2], b"d3")],[Button.inline(day[3], b"d4")],[Button.inline(day[4], b"d5")]]
     await bot.send_message(send_event.query.user_id, 'Выбери день', buttons=kbdays)
-    @bot.on(events.CallbackQuery(data=re.findall(b'd1|d2|d3|d4|d5')))
+    @bot.on(events.CallbackQuery(data=re.compile(b'd1|d2|d3|d4|d5')))
     async def send_commandday_handler(send_event):
         await send_event.answer()
-        await bot.send_message(send_event.query.user_id, 'Выбери жизн')
+        match send_event.data:
+            case b"d1":
+                workday["day"] = day[0]
+            case b"d2":
+                workday["day"] = day[1]
+            case b"d3":
+                workday["day"] = day[2]
+            case b"d4":
+                workday["day"] = day[3]
+            case b"d5":
+                workday["day"] = day[4]
+        kbworkh = [Button.inline("Полный день", b"wf"), Button.inline("Укажу промежуток", b"wnf")]
+        await bot.send_message(send_event.query.user_id, 'Какая длина рабочего дня?', buttons=kbworkh)
+
+        @bot.on(events.CallbackQuery(data=re.compile(b'wf|wnf')))
+        async def send_command_workh_f(send_workh):
+            if send_workh.data == b'wf':
+                workday["startday"] = "10:00"
+                workday["endday"] = "18:30"
+                workday["place"] = "HQ"
+                bot.send_message(send_workh.query.user_id, 'День записан!')
+            else:
+                # Можно сделать красивый ввод в виде кнопок, но потом
+                # Пока что реализую в виде отправки сообщения в формате ЧЧ:ММ
+                #kbshours = [[Button.inline("10:00", b"10:00"), Button.inline("10:30", b"10:30"), Button.inline("11:00", b"11:00"), Button.inline("11:30", b"11:30"), Button.inline("12:00", b"12:00")]
+                           # [Button.inline("12:30", b"12:30"), Button.inline("13:00", b"13:00"), Button.inline("13:30", b"13:30"), Button.inline("14:00", b"14:00"), Button.inline("14:30", b"14:30")]]
+                #await bot.send_message(send_workh.query.user_id, 'Выбери начало рабочего дня', buttons=kbshours)
+                await bot.send_message("Введи час начала рабочего дня в формате ЧЧ:ММ")
 
 try:
     bot.run_until_disconnected()
